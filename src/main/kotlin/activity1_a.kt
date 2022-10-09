749757d9e92650ce9aeb3851089bf75a97f44c60
JALMONTE <jasonjhonalmonte@gmail.com>
2022-10-10 00:37:35 +0800

    Initial Commit

--- src/main/kotlin/activity1_a.kt
+++ src/main/kotlin/activity1_a.kt
@@ -1,4 +1,28 @@
+import java.time.LocalDateTime
+
 fun main(){
+
+    var firstname:String = ""
+    var middlename:String = ""
+    var lastname:String = ""
+    var fullname:String = "$firstname $middlename $lastname"
+    var facebook:String = ""
+    var emailAddress:String = ""
+    var course:String = ""  //MD2P
+    var batch:Int  //2022
+    var progressStatus:Boolean = false  //Pass / Fail / Incomplete
+
+
+    var attendance:Boolean
+    var timeIn = LocalDateTime.now()
+    var grades:Double = 0.0
+
+
+
+
+
+
+
 
 
 
