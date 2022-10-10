2a8e3eaf4a2aff8f2c577b6b34e1e9f711843c46
JALMONTE <jasonjhonalmonte@gmail.com>
2022-10-11 03:07:43 +0800

    Initial Commit

--- src/main/kotlin/activity1_c.kt
+++ src/main/kotlin/activity1_c.kt
@@ -1,19 +1,32 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 fun main(){
 
-//    You are tasked to automate an inventory system for a grocery..
+//    You are tasked to automate an inventory system for a grocery.
 //    Identify the items that can be bought in a grocery store.
 //    After listing the different items, identify the characteristics of the items.
 //    You are also tasked to group the items in categories to help manage the grocery.
 //    Use the proper data types.
 
-    var items = arrayOf("Cigarette","Shampoo","Biscuit","Noodles")
-    var prices = arrayOf("15","50","25")
+    var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+    var dateToday = LocalDateTime.now().format(formatter)
 
+    //
+    var dateNow = dateToday
     var itemCode:String = ""
     var itemDescription:String = ""
     var itemCategory:String = ""
-    var quantity:Int
-    var stocks:Int
+    var quantity = 0
+    var stocks = 0
+
+    if (stocks == 0){
+        println("Not enough stocks")
+    }else{
+        stocks -= quantity
+        println("Successful!, New stock $stocks")
+    }
+
 
 
 
