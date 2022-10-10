41a2455b97283aa18d77d5d048a129ea9d46e5db
JALMONTE <jasonjhonalmonte@gmail.com>
2022-10-11 02:56:48 +0800

    Initial Commit

--- src/main/kotlin/activity1_c.kt
+++ src/main/kotlin/activity1_c.kt
@@ -6,34 +6,16 @@ fun main(){
 //    You are also tasked to group the items in categories to help manage the grocery.
 //    Use the proper data types.
 
-
     var items = arrayOf("Cigarette","Shampoo","Biscuit","Noodles")
     var prices = arrayOf("15","50","25")
 
-    var names = arrayOf(items, prices)
+    var itemCode:String = ""
+    var itemDescription:String = ""
+    var itemCategory:String = ""
+    var quantity:Int
+    var stocks:Int
 
 
 
-    var userName:String? = null
-    var needAnyhelp:String? = null
-
-    print("Enter your name: ")
-    userName = readln().toString()
-    println("Do you need any help? ")
-    needAnyhelp = readln().toString()
-
-    var plusplus = 0
-    plusplus += 5
-    println(++plusplus)
-
-    if (needAnyhelp.lowercase() == "y"){
-        println("How may i help you?")
-    }else if (needAnyhelp.lowercase() == "n") {
-        println("Type n")
-    }
-    else {
-        println("oh okay ...")
-    }
-    println("Thank ypu very much")
 
 }
--- src/main/kotlin/activity1_d.kt
+++ src/main/kotlin/activity1_d.kt
@@ -1,3 +1,9 @@
+
+//Create an application that will accept 5 monetary amounts.
+//After the input is done, the user will be asked “Divide the value by how many?”.
+//It will only accept an Integer as input.
+//The total of the 5  input will be divided by the answer in the second question input.
+
 fun main(){
     var monetaryAmount = 0
 
