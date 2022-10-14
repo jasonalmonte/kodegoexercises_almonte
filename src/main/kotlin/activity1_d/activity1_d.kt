9756c01455781ed6874e58e30c8b3d02882e55b3
JALMONTE <jasonjhonalmonte@gmail.com>
2022-10-14 20:04:48 +0800

    Initial Commit

--- src/main/kotlin/activity1_d/activity1_d.kt
+++ src/main/kotlin/activity1_d/activity1_d.kt
@@ -1,3 +1,5 @@
+import javax.swing.text.StyledEditorKit.BoldAction
+import kotlin.system.exitProcess
 
 //Create an application that will accept 5 monetary amounts.
 //After the input is done, the user will be asked “Divide the value by how many?”.
@@ -5,18 +7,48 @@
 //The total of the 5  input will be divided by the answer in the second question input.
 
 fun main(){
-    var monetaryAmount = 0
+    var monetaryAmount:String = ""
     var result:Double = 0.0
+    var number = 48 .. 57
+    var int :Boolean = false
 
     println("Please input 5 monetary amount: ")
-    monetaryAmount = readln().toInt()
+    monetaryAmount = readln().toString()
+
+    for(counter in 0 until  monetaryAmount.length){
+        var char = monetaryAmount[counter].toFloat()
+
+        if(char.toInt() in 48 .. 57){
+            println("Its Int: $monetaryAmount")
+            int = true
+        }else{
+            println("Not Int: ${char.toInt()}")
+
+        }
+    }
+
+    if (int){
+        print("Divide the value by how many? ")
+//        monetaryAmount /= readln().toInt()
+//        result = monetaryAmount.toDouble()
+//        println("Answer: $result")
+    }else{
+        println("error")
+    }
+
+    do {
+        var jason = ""
+    }while (true)
+
+
+
     monetaryAmount += readln().toInt()
     monetaryAmount += readln().toInt()
     monetaryAmount += readln().toInt()
     monetaryAmount += readln().toInt()
 
     print("Divide the value by how many? ")
-    monetaryAmount /= readln().toInt()
+   // monetaryAmount /= readln().toInt()
     result = monetaryAmount.toDouble()
     println("Answer: $result")
 
@@ -28,7 +60,7 @@ fun main(){
     }
 
     print("Divide the value by how many? ")
-    monetaryAmount /= readln().toInt()
+  //  monetaryAmount /= readln().toInt()
     result = monetaryAmount.toDouble()
     println("Answer: $result")
 
