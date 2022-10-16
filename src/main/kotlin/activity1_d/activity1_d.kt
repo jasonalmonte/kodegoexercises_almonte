6042262e1f9d705e1a10e2b51ded5c962cf67844
JALMONTE <jasonjhonalmonte@gmail.com>
2022-10-16 21:32:37 +0800

    Initial Commit

--- src/main/kotlin/activity1_d/activity1_d.kt
+++ src/main/kotlin/activity1_d/activity1_d.kt
@@ -5,46 +5,68 @@ import mu.KotlinLogging
 //The total of the 5  input will be divided by the answer in the second question input.
 private val logger = KotlinLogging.logger{}
 fun main(){
-    var monetaryAmount:String = ""
+    var userInput:String = ""
+    var monetaryAmount = 0
     var result:Double = 0.0
     var number = 48 .. 57
     var negativeValue:Boolean = false
     var foundInteger :Boolean = false
 
-    logger.info { "Please input 5 monetary amount: " }
-    monetaryAmount = readln().toString()
 
     for(input in 0 until 5){
-        println("$input")
-    }
 
-    //CHECK ALL CHAR
-    for(counter in 0 until  monetaryAmount.length){
-        var char = monetaryAmount[counter].toFloat()
+        foundInteger = false
+        negativeValue = false
 
-        if(char.toInt() in number){
-            //println("Its Int: $monetaryAmount")
-            logger.info { "Its Int: $monetaryAmount" }
-            foundInteger = true
-        }else if(char.toInt() == 45){
-            logger.info { "Negative value, proceed line.." }
-            negativeValue = true
-        }else{
-            logger.info { "Not Int: ${char.toInt()}" }
-            foundInteger = false
-            break
+        logger.info { "[$input] Please input 5 monetary amount: " }
+        userInput = readln().toString()
+
+        //Check per char
+        for(counter in 0 until  userInput.length){
+            var char = userInput[counter].toFloat()
+
+            if (char.toInt() == 45) {
+                negativeValue = true
+                logger.info { "Negative value, proceed line.." }
+            }else if(char.toInt() in number){
+                    logger.info { "Its Int: ${userInput[counter]}" }
+                    foundInteger = true
+            }else {
+                    logger.info { "Not Int: ${userInput[counter]}" }
+                    foundInteger = false
+                    break
+            }
         }
-    }
 
-    if (foundInteger){
-        monetaryAmount.toInt()
-        logger.info { "Divide the value by how many? " }
+        if (foundInteger){
+            if (negativeValue){
+                monetaryAmount = -userInput.toInt()
+            }else{
+                monetaryAmount = userInput.toInt()
+            }
+            logger.info { "Userinput: $monetaryAmount" }
+            logger.info { "Divide the value by how many? " }
 //        monetaryAmount /= readln().toInt()
 //        result = monetaryAmount.toDouble()
 //        println("Answer: $result")
-    }else{
-        logger.info { "Invalid input" }
+        }else{
+            logger.info { "Invalid input" }
+            break
+        }
+
     }
+
+
+
+
+    }
+
+    //CHECK ALL CHAR
+
+
+
+
+
 
 //
 //    monetaryAmount += readln().toInt()
@@ -69,5 +91,3 @@ fun main(){
 //    result = monetaryAmount.toDouble()
 //    println("Answer: $result")
 
-
-}
