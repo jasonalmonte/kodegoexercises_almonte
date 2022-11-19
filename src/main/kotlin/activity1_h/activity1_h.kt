1f3c36450e5b9e627865d602653b518ea643d892
JALMONTE <jasonjhonalmonte@gmail.com>
2022-11-20 01:01:38 +0800

    Initial Commit

--- src/main/kotlin/activity1_h/activity1_h.kt
+++ src/main/kotlin/activity1_h/activity1_h.kt
@@ -1,78 +1,41 @@
 package activity1_h
 
 fun main(){
-    var intA: Int = 5
-    var primeNumber: Boolean = true
+    var firstValue: Int = 1
+    var secondValue: Int = 5
     var primeList: ArrayList<Int> = ArrayList()
-    var equalBa: Boolean = false
-    var count: Int = 0
+    var howManyNaturalNumber: Int = 0
 
-   var allNumber: ArrayList<Int> = ArrayList()
-    var naturalNumber = 0..100
-
-    intA = readln().toInt()
-    var xTwo = 0 .. 100 step(2)
-    var xThree = 0 .. 100 step(3)
-
-    for(x in naturalNumber){
-        allNumber.add(x)
-    }
-
-    for(y in allNumber.indices){
-        //println(allNumber[y])
-    }
+    firstValue = readln().toInt()
+    secondValue = readln().toInt()
+    var allNumbers = 0..secondValue
 
 
-    if(intA in xTwo){
-        allNumber.removeAt(5)
-    }else if(intA in xThree){
-        primeNumber = true
-    }
 
-    for(getPrime in naturalNumber){
-        //println(getPrime)
-        for(a in naturalNumber){
-            for(b in naturalNumber){
-                //println("$a x $b = ${a * b} $count")
-                if(intA == a * b){
-                    //count ++
+
+    for(getPrimeNumber in allNumbers){
+        for(a in allNumbers){
+            for(b in allNumbers){
+
+                if(getPrimeNumber == a * b){
+                    howManyNaturalNumber ++
                     //println("$a x $b = ${a * b} $count")
-                    equalBa = true
-                }
-                if(getPrime == a * b){
-                    count ++
-                    //println("$a x $b = ${a * b} $count")
-                    equalBa = true
                 }
             }
         }
 
-        if (count >= 2){
-            primeNumber = false
-        }
-
-
-        if((count <= 2) && (getPrime <= intA)){
-            primeList.add(getPrime)
-           // println("Prime Number $getPrime")
+        if((howManyNaturalNumber <= 2) && ( getPrimeNumber >= firstValue) && (getPrimeNumber <= secondValue)){
+            primeList.add(getPrimeNumber)
+           // println("Prime Number $getPrimeNumber")
         }else{
            // println("Not Prime Number")
         }
 
-        count = 0
-
-
+        howManyNaturalNumber = 0
     }
 
+    print("1,$secondValue -> ")
     for(printPrimeList in primeList.indices){
-        println("Prime Number ${primeList[printPrimeList]}")
+        print("${primeList[printPrimeList]} ")
     }
-
-
-
-
-
-
-    primeNumber = false
-
 }
