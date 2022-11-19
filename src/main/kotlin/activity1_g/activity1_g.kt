887d80c0b6481a2dc10ad7fbda53cdad81bc961e
JALMONTE <jasonjhonalmonte@gmail.com>
2022-11-19 22:53:05 +0800

    Initial Commit

--- src/main/kotlin/activity1_g/activity1_g.kt
+++ src/main/kotlin/activity1_g/activity1_g.kt
@@ -1,13 +1,19 @@
 package activity1_g
 
 fun main(){
-    var String1: String = ""
+    var string1: String = ""
+    var storeString1: String = ""
+    var palindrome: Boolean = false
 
     println("Input string:")
-    String1 = readln()
+    string1 = readln()
+    for(char in 0 until string1.length){
+        storeString1 += string1[char]
 
-    for(char in 0 .. String1.length){
-        println(String1[char])
+        if(string1[char] != string1[(string1.length - char) -1]) {
+            palindrome = true
+        }
+        println("A: ${string1[char]} B: ${string1[(string1.length - char) -1]}")
     }
 
 }
