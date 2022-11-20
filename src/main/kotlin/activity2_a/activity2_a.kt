371bb58a6780422a49e3d4322e3d30e31ff4bf9f
JALMONTE <jasonjhonalmonte@gmail.com>
2022-11-20 23:09:30 +0800

    Initial Commit

---
+++ src/main/kotlin/activity2_a/activity2_a.kt
@@ -0,0 +1,9 @@
+package activity2_a
+
+fun main(){
+    var studentEmail = ArrayList<String>()
+    var studentContacts = ArrayList<String>()
+    var studentList = ArrayList<String>()
+    var studentOverall = ArrayList<String>()
+    var studentStatus = ArrayList<Boolean>()
+}
