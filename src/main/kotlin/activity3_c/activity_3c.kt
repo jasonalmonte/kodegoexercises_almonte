374c207f4e51e2e92c3782d870a89eb8a5b50ad2
JALMONTE <jasonjhonalmonte@gmail.com>
2022-11-22 01:07:59 +0800

    Initial Commit

--- src/main/kotlin/activity3_c/activity_3c.kt
+++ src/main/kotlin/activity3_c/activity_3c.kt
@@ -7,17 +7,43 @@ fun main(){
         "Alfonso", "Ice", "Vinegar", "Pepper", "Garlic")
 
     var cart : HashMap<String, Int> = HashMap()
+    var inputProduct = readln().toString()
 
+    addToCart("Winston",1, cart)
+    addToCart("Marlboro",1, cart)
+    addToCart("Marlboro",1, cart)
 
+    //addToCart(inputProduct,1,cart)
+    checkOut(cart)
+    removeFromCart(inputProduct,cart)
 
+    for(y in cart.entries){
+        println("${y.key} ${y.value}")
+    }
 }
 
-fun addToCart(){
+fun addToCart(inputProduct: String, quantity: Int, cart: HashMap<String, Int>,) {
+    var countExistingName: Int = 0
 
+    for (x in cart.entries) {
+        if (inputProduct == x.key) {
+            countExistingName++
+        }
+    }
+    if (countExistingName > 0) {
+        cart["${inputProduct}_${countExistingName}"] = quantity
+        println("[2] Add to cart : ${inputProduct}_${countExistingName}")
+    } else {
+        cart[inputProduct] = quantity
+        println("[2] Add to cart : $inputProduct")
+    }
+    countExistingName = 0
 }
-fun checkOut(){
 
+fun checkOut(cart: HashMap<String, Int>,){
+    println("[3] Checkout : ${cart.count()} Items")
 }
-fun removeFromCart(){
-
+fun removeFromCart(inputProduct: String, cart: HashMap<String, Int>,){
+    cart.remove(inputProduct)
+    println("[4] Remove from cart : $inputProduct")
 }
