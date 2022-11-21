778905bbc8a47bce10b3e3c2dbb680846cc00105
JALMONTE <jasonjhonalmonte@gmail.com>
2022-11-22 00:04:14 +0800

    Initial Commit

--- src/main/kotlin/activity3_c/activity_3c.kt
+++ src/main/kotlin/activity3_c/activity_3c.kt
@@ -1,5 +1,23 @@
 package activity3_c
 
 fun main(){
+    var names : ArrayList<String> = arrayListOf("Nescafe", "Marlboro", "Gardenia", "Winston", "Safeguard",
+        "Palmolive", "Johnson Powder", "Coke", "Royal", "Knorr",
+        "Candle", "Champion", "Sprite", "Gatorade", "Red Horse",
+        "Alfonso", "Ice", "Vinegar", "Pepper", "Garlic")
+
+    var cart : HashMap<String, Int> = HashMap()
+
+
+
+}
+
+fun addToCart(){
+
+}
+fun checkOut(){
+
+}
+fun removeFromCart(){
 
 }
