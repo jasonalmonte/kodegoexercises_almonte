be64f32ec5e43aed9f671dabea317681d0fddce6
JALMONTE <jasonjhonalmonte@gmail.com>
2022-11-23 19:11:42 +0800

    Initial Commit

---
+++ src/main/kotlin/activity4_c/activity4_c.kt
@@ -0,0 +1,97 @@
+package activity4_c
+
+fun main(){
+
+}
+
+class Poultry{
+    var itemName:String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Fish{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class CannedGoods{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""}
+class Snacks{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class FrozenProducts{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Fruits{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Vegetables{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Condiments{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Furniture{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Appliances{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Clothes{
+    var itemName: String = ""
+    var size: Int = 0
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Pet{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class SanitaryProducts{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class PersonalCare{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+class Toys{
+    var itemName: String = ""
+    var price: Float = 0.0F
+    var qty: Double = 0.0
+    var unit: String = ""
+}
+
