3b0dc60295bc75fa7c91c051f9289b796426b661
JALMONTE <jasonjhonalmonte@gmail.com>
2022-12-01 20:28:14 +0800

    Initial Commit

--- src/main/kotlin/activity5_d/activity5_d.kt
+++ src/main/kotlin/activity5_d/activity5_d.kt
@@ -3,6 +3,12 @@ package activity5_d
 open class Players{
     var name: String = ""
     var color: PlayerColor = PlayerColor.RED
+
+    constructor(name: String, color: PlayerColor){
+        this.name = name
+        this.color = color
+    }
+
 }
 
 enum class PlayerColor{
@@ -12,14 +18,43 @@ enum class PlayerColor{
     YELLOW,
 }
 
-class Traps : Players(){
-    var snake: ArrayList<String> = ArrayList()
+
+class Traps (name: String, color :PlayerColor): Players(name, color){
+    //var snake: Array<Double> = arrayOf(-5.0,-2.0,-4.0,-10.0)
     var location: HashMap<Players,Int> = HashMap()
-    fun move(){  }
+    var ladder: HashMap<Double,Int> = HashMap()
+    var playerList: ArrayList<Players> = ArrayList()
+
+    private var characters: Map<Int, Double> = mutableMapOf((10 to -5.0), (25 to -5.0), (37 to -20.0),(53 to -10.0),
+                                                            (67 to -25.0), (78 to -16.0), (81 to -25.0),(99 to -71.0))
+    var snake = characters as HashMap<Int,Double>
+
+    fun addPlayers(){
+        playerList.add(Players("Player1", PlayerColor.RED))
+    }
+    fun haveTrap(){
+        for(entries in snake.entries){
+            if(81 == entries.key){
+                println(entries.value)
+            }
+        }
+    }
+    fun move(){
+        haveTrap()
+    }
+
+}
+class OnBoard{
+
 }
 
-class Reward : Players(){
-    var ladder: ArrayList<String> = ArrayList()
+fun startAutoPlay(){
+
+}
+
+class Reward : Players("Player2", PlayerColor.BLACK){
+   // var board: Array<Int> = Array(100, init = 0)
+   // var ladder: ArrayList<String> = ArrayList()
     var moves: ArrayList<String> = ArrayList()
     fun move(){  }
 }
@@ -31,10 +66,11 @@ fun diceRoll(): Int{
 fun main(){
 // prints new sequence every time
     println(diceRoll())
-
-    var trap = Traps()
-
-    trap.location.keys
-
-
-}
+var traps = Traps("Player1", PlayerColor.RED)
+    traps.move()
+    var trap:Players = Players("Player1", PlayerColor.RED)
+    //Join Player
+    //Start
+    //Roll -> Rules -> Move
+    //Next Player
+    }
