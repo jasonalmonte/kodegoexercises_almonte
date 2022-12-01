55e7bc17ef6e3b4abdd4caa7778f10daf2b789b8
JALMONTE <jasonjhonalmonte@gmail.com>
2022-12-01 21:31:52 +0800

    Initial Commit

--- src/main/kotlin/activity5_d/activity5_d.kt
+++ src/main/kotlin/activity5_d/activity5_d.kt
@@ -19,15 +19,19 @@ enum class PlayerColor{
 }
 
 
-class Traps (name: String, color :PlayerColor): Players(name, color){
-    //var snake: Array<Double> = arrayOf(-5.0,-2.0,-4.0,-10.0)
-    var location: HashMap<Players,Int> = HashMap()
-    var ladder: HashMap<Double,Int> = HashMap()
-    var playerList: ArrayList<Players> = ArrayList()
+class Rules (name: String, color :PlayerColor): Players(name, color){
 
-    private var characters: Map<Int, Double> = mutableMapOf((10 to -5.0), (25 to -5.0), (37 to -20.0),(53 to -10.0),
-                                                            (67 to -25.0), (78 to -16.0), (81 to -25.0),(99 to -71.0))
-    var snake = characters as HashMap<Int,Double>
+    var playerList: ArrayList<Players> = ArrayList()
+    var location: HashMap<Players,Int> = HashMap()
+
+    private var ladderMap: Map<Int, Double> = mutableMapOf((10 to -5.0), (25 to -5.0), (37 to -20.0),(53 to -10.0),
+        (67 to -25.0), (78 to -16.0), (81 to -25.0),(99 to -71.0))
+    private var snakeMap: Map<Int, Double> = mutableMapOf((10 to -5.0), (25 to -5.0), (37 to -20.0),(53 to -10.0),
+        (67 to -25.0), (78 to -16.0), (81 to -25.0),(99 to -71.0))
+
+    var ladder = ladderMap as HashMap<Int,Double>
+    var snake = snakeMap as HashMap<Int,Double>
+
 
     fun addPlayers(){
         playerList.add(Players("Player1", PlayerColor.RED))
@@ -39,16 +43,51 @@ class Traps (name: String, color :PlayerColor): Players(name, color){
             }
         }
     }
-    fun move(){
-        haveTrap()
+    fun move(player: Players, position: Int, dice: Int){
+        for(index in location.entries){
+            if(player.name == index.key.name){
+
+                println(index.key.name)
+            }
+        }
+        location[player] = position
+        snakeAndLadder()
     }
+    fun snakeAndLadder(){
+        for(entries in snake.entries){
+            if(81 == entries.key){
+                println(entries.value)
+                //move + loc next turn
+            }
+        }
+        for(entries in ladder.entries){
+            if(81 == entries.key){
+                println(entries.value)
+                //move + loc next turn
+            }
+        }
+    }
+
+
+
 
 }
 class OnBoard{
+    fun diceRoll(): Int{
+        return (1..6).random()
+    }
+
 
 }
 
 fun startAutoPlay(){
+
+
+    var rules = Rules("Player1", PlayerColor.RED)
+    //rules.rolldice
+
+    var dices = OnBoard()
+    rules.move(Players("",PlayerColor.RED),0,dices.diceRoll())
 
 }
 
@@ -59,15 +98,23 @@ class Reward : Players("Player2", PlayerColor.BLACK){
     fun move(){  }
 }
 
-fun diceRoll(): Int{
-    return (1..6).random()
-}
+
 
 fun main(){
 // prints new sequence every time
-    println(diceRoll())
-var traps = Traps("Player1", PlayerColor.RED)
-    traps.move()
+
+    var onboard = OnBoard()
+    var diceRo: Int = onboard.diceRoll()
+    println(diceRo)
+    var rules = Rules("Player1", PlayerColor.RED)
+
+    rules.addPlayers()
+    rules.move(Players("Player1",PlayerColor.RED),2,diceRo)
+
+
+
+
+
     var trap:Players = Players("Player1", PlayerColor.RED)
     //Join Player
     //Start
